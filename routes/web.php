543eb9771b1bb993b38349a2b222c277bb879a24
doryunnim <kdhwan1224@gmail,com>
2019-11-17 21:22:39 +0900

    쓸데없는작업

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,38 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/resources/views/join.blade.php', function(){
+    return view('join');
+});
+
+Route::get('auth/login', function(){
+    $credentials = [
+        'email' => 'john@example.com',
+        'password' => 'password'
+    ];
+    
+    if(!auth()->attempt($credentials)){
+        return '로그인 정보가 정확하지 않습니다.';
+    }
+
+    return '로그인 되었습니다.';
+});
+
+Route::get('protected', function(){
+    dump(session()->all());
+
+    if(! auth()->check()){
+        return '누구세요?';
+    }
+
+    return '어서 오세요' . auth()->user()->name;
+});
+
+Route::get('auth/logout', function(){
+    auth()->logout();
+
+    return '또 봐요~!';
+});
+
