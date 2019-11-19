596c2ab11685f82c0a2cb846355aefc412dc20a9
owowow <43901551+owowa@users.noreply.github.com>
2019-11-19 11:00:23 +0900

    modified main page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -26,11 +26,41 @@
                     <a class="nav-link js-scroll-trigger" href="{{route('articles.index')}}">현지학기제</a>
                 </li>                
                 <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#mainNav">QnA</a>
-                </li>  
+                    <a class="nav-link js-scroll-trigger" href="#mainNav">Q&amp;A</a>
+                </li>
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                       document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+<!--
                 <li class="nav-item auth">
                     <a class="nav-link js-scroll-trigger" href="{{route('login')}}">로그인/로그아웃</a>
                 </li>                
+-->
             </ul>
         </div>
       </div>
