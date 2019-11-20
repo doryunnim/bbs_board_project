d4f62fddc1c3bc7c6099d01661d71880bccad1da
doryunnim <kdhwan1224@gmail,com>
2019-11-20 20:31:25 +0900

    데이터베이스 시딩

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,12 +16,23 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'email' => $faker->safeEmail,
+        'password' => bcrypt('password'),
         'remember_token' => Str::random(10),
     ];
 });
+
+$factory->define(App\Qna_article::class, function (Faker $faker) {
+    $date = $faker->dateTimeThisMonth;
+
+    return [
+        'title' => $faker->sentence(),
+        'content' => $faker->paragraph(),
+        'created_at' => $date,
+        'updated_at' => $date,
+    ];
+});
+
