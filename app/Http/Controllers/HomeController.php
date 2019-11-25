f48a4a884993498737d0f3e52281d578b1924a12
TFGK <deee5@naver.com>
2019-11-25 15:57:33 +0900

    [php] main page CSS보완 필요

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,8 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
-        return view('home');
+    {   
+        
+        return view('mainpage');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
+Auth::routes();
 
-# 127.0.0.1:8000 들어가면 나오는 메인페이지
+# 127.0.0.1:8000 들어가면 로그인 창
 Route::get('/', function () {
     return view('auth.login');
 });
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index');
 
 Route::resource('introduce','NabeIntroduceController');
