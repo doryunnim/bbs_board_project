5c8cf9d91e345e5566a059dce1994de7e3cb862b
TFGK <deee5@naver.com>
2019-11-29 01:38:17 +0900

    introduce CRUD + index.blade 수정중

--- app/Http/Controllers/NabeIntroduceController.php
+++ app/Http/Controllers/NabeIntroduceController.php
@@ -23,29 +23,6 @@ class NabeIntroduceController extends Controller
 
     public function store(Request $request)
     {
-        // #file에 사진이 있는지 확인하고 없으면 에러
-        // if(!$request->file('photo')) {
-        //     return redirect()->back()->withErrors([
-        //         'error' => "사진을 업로드 하세요"
-        //     ]);
-        // }
-        
-        // #Store
-        // $path = $request->file('photo')->store('public');
-
-        // #사진넣기
-        // NabeIntroduce::create([
-        //     'name' => $request->name,
-        //     'comment' => $request->comment,
-        //     'url' => Storage::url($path),
-
-        // ]);
-
-        #사진 처리 완료되면 실행됨
-        #return redirect(route('introduces.index'));
-        #========================================================
-
-        #file에 사진이 있는지 확인하고 없으면 에러
         if(!$request->file('photo')) {
             return redirect()->back()->withErrors([
                 'error' => "사진을 업로드 하세요"
@@ -94,8 +71,7 @@ class NabeIntroduceController extends Controller
 
         return redirect()->route('introduces.index');
     }
-
-    #아 디스트로이가 안되는데 
+     
     public function destroy(NabeIntroduce $introduce)
     {
         $introduce->delete();
--- resources/views/introduce/index.blade.php
+++ resources/views/introduce/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<br><br><br>
 <div class="container">
     <div class="page-header">
         <h3>Introduce My teams</h3>
@@ -10,19 +9,28 @@
     <div class="row">
         <!-- 조원 사진 & 추가 부분 -->
         @foreach($introduces as $introduce)
-        <div class="col-md-1">
-            <img src="{{ $introduce->url }}" class="img-fluid">
-            <h4>{{ $introduce->name }}</h4>
+        <div class="col-md-2">
+            <div class = "w3-container">
+                <div class="w3-card" style="width:150px; height:150px">
+                    <img src="{{ $introduce->url }}" alt="Person" style="width:150px; height:118px">
+                    <div class = "w3-container"> 
+                        <h4>{{ $introduce->name }}</h4>
+                    </div>
+                </div>
+            </div>
+            <br>
         </div>
         @endforeach
         <a href="{{ route('introduces.create') }}">ADD</a>
     </div>
-    <hr>
-    <div>
+    <div class="container" style="text-align:center">
         <h2>우리 조는 말이죠</h2>
+        <h2>우리 조는 말이죠</h2>
+        <h2>우리 조는 말이죠</h2>
+        <h2>우리 조는 말이죠</h2>
+        <hr>
     </div>
-    <hr>
-    <div class="row">
+    <div class="row"> 
         @foreach($introduces as $introduce)
             @include('introduce.partial.introduce')
         @endforeach
