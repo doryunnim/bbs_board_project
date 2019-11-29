65f41f30800da54b1dafd105ee46964e31c92986
TFGK <deee5@naver.com>
2019-11-29 21:56:58 +0900

    introduce, Q&A CRUD and 대댓글

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,8 @@ Auth::routes();
 Route::get('/', function () {
     return view('main');
 });
-
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-
 
 Route::resource('articles','NabeJapanController');
 Route::resource('qnaArticles','QnaArticlesController');
