6cea8bec3066f036e1d93d9212e37c4708258e8f
ChoiMinJun <zkxm008@naver.com>
2019-12-08 14:33:36 +0900

    all +

--- database/migrations/2019_11_23_204251_create_nabe_introduces_table.php
+++ database/migrations/2019_11_23_204251_create_nabe_introduces_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 class CreateNabeIntroducesTable extends Migration
 {
     /**
@@ -18,13 +16,11 @@ class CreateNabeIntroducesTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->string('comment');
-            $table->string('url');
+            $table->string('image');
             $table->timestamps();
-
             $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
         });
     }
-
     /**
      * Reverse the migrations.
      *
